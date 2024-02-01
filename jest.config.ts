8feb12f0bb376bf25a89fd289a42285717d792db
Juan Camilo Bazurto <juan.bazurto@mail.escuelaing.edu.co>
2024-02-01 13:52:03 -0500

    sonarqube

--- jest.config.ts
+++ jest.config.ts
@@ -15,6 +15,7 @@ const config: Config = {
     "^@/components/(.*)$": "<rootDir>/components/$1",
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testMatch: ["<rootDir>/**/*.spec.ts", "<rootDir>/**/*.spec.tsx"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
